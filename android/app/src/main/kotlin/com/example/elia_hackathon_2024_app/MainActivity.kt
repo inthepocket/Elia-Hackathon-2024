892914a565ea511d355b0dca383b8392bd4d7c10
Bram De Coninck <bramdeconinck3@gmail.com>
2024-03-26 13:33:44 +0100

    Switch to Flutter 3.19.x

--- android/app/src/main/kotlin/com/example/elia_hackathon_2024_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/elia_hackathon_2024_app/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.elia_hackathon_2024_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
