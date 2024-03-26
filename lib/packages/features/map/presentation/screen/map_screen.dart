5e4e4d7f2931822ebcbafda06e7243f44d2e00d6
Bram De Coninck <bramdeconinck3@gmail.com>
2024-03-26 15:34:50 +0100

    Add cancellable network tile provider and switch back to Flutter 3.16.3

--- lib/packages/features/map/presentation/screen/map_screen.dart
+++ lib/packages/features/map/presentation/screen/map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:latlong2/latlong.dart';
 
@@ -20,6 +21,7 @@ class MapScreen extends StatelessWidget {
           TileLayer(
             urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
             userAgentPackageName: 'com.inthepocket.eliaHackathon2024App',
+            tileProvider: CancellableNetworkTileProvider(),
           ),
           CurrentLocationLayer(
             alignPositionOnUpdate: AlignOnUpdate.always,
